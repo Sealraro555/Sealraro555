46157ea66276b5efa2e7df142a3e0224bc592d9b
Sealraro555 <sealraro555@gmail.com>
2024-08-15 22:37:03 -0500

    Update discord_bot_version2.py

--- discord_bot_version2.py
+++ discord_bot_version2.py
@@ -1,6 +1,8 @@
 import discord
+import os
 from discord.ext import commands
 import random
+import requests
 
 intents = discord.Intents.default()
 intents.members = True
@@ -10,7 +12,7 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print(f'Iniciado sesión como {bot.user} (ID: {bot.user.id})')
     print('------')
 
 @bot.command()
@@ -23,16 +25,60 @@ async def heh(ctx, count_heh = 5):
 
 @bot.command()
 async def add(ctx, left: int, right: int):
-    """Adds two numbers together."""
     await ctx.send(left + right)
+    print('Esa es la suma de los numeros que escribiste')
 
 @bot.group()
 async def cool(ctx):
-    """Says if a user is cool.
-
-    In reality this just checks if a subcommand is being invoked.
-    """
     if ctx.invoked_subcommand is None:
-        await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
+        await ctx.send(f'Yes, {ctx.subcommand_passed} is cool')
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+@bot.command()
+async def imagen1(ctx):
+    with open('IMAGENES/imagen1.jpg', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+@bot.command()
+async def imagen2(ctx):
+    imagenes = os.listdir('IMAGENES')
+    with open(f'IMAGENES/{random.choice(imagenes)}', 'rb') as f:
+            picture = discord.File(f)
+    await ctx.send(file=picture)
+
+@bot.command()
+async def imagen3(ctx):
+    with open('IMAGENES/imagen4.jpg', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Una vez que llamamos al comando duck, 
+    el programa llama a la función get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+@bot.command()
+async def animal(ctx):
+    animales = os.listdir('ANIMALES')
+    with open(f'ANIMALES/{random.choice(animales)}', 'rb') as f:
+            picture = discord.File(f)
+    await ctx.send(file=picture)
 
 bot.run("bot token")
